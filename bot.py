4acc7186503b41f21b732d03becf4ad40e42445e
snivinnn <snivinsanthosh@gmail.com>
2024-10-11 11:30:28 +0000

    initial commit

---
+++ bot.py
@@ -0,0 +1,21 @@
+import logging
+import telegram
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+app = ApplicationBuilder().token("7566081485:AAESjUfs31Aa3zAke2GIN04fr6Ao-Qe3-uM").build()
+
+async def start(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id = update.effective_chat.id, text = "Ishan is a nigga")
+
+start_handler = CommandHandler("start", start)
+app.add_handler(start_handler)
+
+app.run_polling()
+
+
